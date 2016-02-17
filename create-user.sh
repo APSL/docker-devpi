036a43296657fbb11d6686a6543fdc7aa59ed037
Edu Herraiz Aparicio <gshark@gmail.com>
2016-02-17 11:09:47 +0100

    Create/modify user fixed

--- create-user.sh
+++ create-user.sh
@@ -1,11 +1,25 @@
 #!/bin/bash
 
+echo "[RUN] Create user or modify password in htpasswd"
 echo "[RUN] htpasswd -b /data/htpasswd $1 $2"
 htpasswd -b /data/htpasswd $1 $2
-echo "[RUN] devpi use http://root:${DEVPI_PASSWORD}@localhost:8000"
-devpi use http://root:${DEVPI_PASSWORD}@localhost:8000
+
+echo "[RUN] Login to devpi with root user"
+URL=`echo $OUTSIDE_URL |sed 's/https\?:\/\///'`
+PROTOCOL=`echo $OUTSIDE_URL| awk -F: '{print $1}'`
+echo "[RUN] devpi use ${PROTOCOL}://root:${DEVPI_PASSWORD}@${URL}"
+devpi use ${PROTOCOL}://root:${DEVPI_PASSWORD}@${URL}
 devpi login root --password="${DEVPI_PASSWORD}"
-echo "[RUN] devpi user -c $1 password='$2'"
-devpi user -c $1 password="$2"
 
 
+USER_EXISTS=`devpi user -l|grep -Fx $1|wc -l`
+if [[ $USER_EXISTS == 1 ]]
+then
+  echo "[RUN] Modifying user $1 in devpi"
+  echo "[RUN] devpi user -m $1 password='$2'"
+  devpi user -m $1 password="$2"
+else
+  echo "[RUN] Create user $1 in devpi"
+  echo "[RUN] devpi user -c $1 password='$2'"
+  devpi user -c $1 password="$2"
+fi
